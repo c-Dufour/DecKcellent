ededefe7f37af610338e6df316a71b38e0da3710
Cyril Dufour <duf.cyril@gmail.com>
2016-12-28 15:55:06 +0100

    add unirest
    utilisation de l'api HS depuis marketplace

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var unirest = require('unirest');
 var app = express();
+
+
+
 
 app.get("/",function(req,res){
   res.sendFile(__dirname+'/index.html');
 });
+
+app.get("/all",function(req,res){
+  unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards")
+  .header("X-Mashape-Key", "C6WrgQzmpVmshR7OeckimjUHcg1Yp1wBOipjsnX0U0ukazfOnS")
+  .end(function (result) {
+    console.log(result.status, result.headers, result.body);
+  });
+//   unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/kazakus")
+// .header("X-Mashape-Key", "C6WrgQzmpVmshR7OeckimjUHcg1Yp1wBOipjsnX0U0ukazfOnS")
+// .end(function (result) {
+//   console.log(result.status, result.headers, result.body);
+// });
+  res.sendFile(__dirname+'/view/bidon.html');
+})
 
 var port = 8084;
 app.listen(port,function(){
